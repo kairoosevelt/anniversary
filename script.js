2bda2f023f11d25aca7aad147542ffaba932390c
Shandy Riski Aditya <kayzeeakun17@gmail.com>
2026-01-13 21:11:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,53 @@
 /* PASSWORD */
-const PASSWORD = "150425";
+const PASSWORD = "bebe";
 
-function checkPassword(){
+function checkPassword() {
   const input = document.getElementById("passwordInput").value;
-  if(input === PASSWORD){
+  if (input === PASSWORD) {
     document.getElementById("password-screen").classList.add("hidden");
     document.getElementById("enter-screen").classList.remove("hidden");
   } else {
-    alert("Wrong password 🤍");
+    alert("wrong password 🤍");
   }
 }
 
 /* ENTER SITE */
-function enterSite(){
+function enterSite() {
   document.getElementById("enter-screen").classList.add("hidden");
-  document.querySelectorAll(".hidden-content").forEach(el => el.style.display="block");
-  document.getElementById("bg-music").play();
+  document.querySelectorAll(".hidden-content").forEach(el => {
+    el.style.display = "block";
+  });
 
+  document.getElementById("bg-music").play();
   startHearts();
 }
 
 /* COUNTER */
 const startDate = new Date("2025-04-15");
 const today = new Date();
-const diffDays = Math.floor((today - startDate)/(1000*60*60*24));
-document.getElementById("counter").innerText = `${diffDays} days of loving you 💕`;
+const diffDays = Math.floor((today - startDate) / (1000 * 60 * 60 * 24));
+document.getElementById("counter").innerText =
+  `${diffDays} days of loving you 💕`;
 
 /* FLOATING HEARTS */
-function startHearts(){
-  setInterval(()=>{
+function startHearts() {
+  setInterval(() => {
     const heart = document.createElement("div");
     heart.className = "heart";
     heart.innerHTML = "❤";
-    heart.style.left = Math.random()*100+"vw";
-    heart.style.animationDuration = 5+Math.random()*5+"s";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = 5 + Math.random() * 6 + "s";
     document.body.appendChild(heart);
-    setTimeout(()=>heart.remove(),10000);
-  },500);
+    setTimeout(() => heart.remove(), 10000);
+  }, 500);
 }
 
 /* IMAGE MODAL */
-function openModal(img){
+function openModal(img) {
   document.getElementById("modal-img").src = img.src;
   document.getElementById("image-modal").style.display = "flex";
 }
 
-function closeModal(){
+function closeModal() {
   document.getElementById("image-modal").style.display = "none";
 }
